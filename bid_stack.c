50c11923af1b7a0f8bb3671c32a3d91749828388
Samuel Mouriño <153455065+samuelmourino@users.noreply.github.com>
2025-04-02 13:50:53 +0200

    Update bid_stack.c

--- bid_stack.c
+++ bid_stack.c
@@ -2,45 +2,40 @@
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
  * AUTHOR 1: Samuel Mouriño Cernadas LOGIN 1: samuel.mourino@udc.es
- * AUTHOR 2: Pedro Gómez Osorio LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 2: Pedro Gómez Osorio LOGIN 2: pedro.gomez.osorio@udc.es
+ * GROUP: 2.3
+ * DATE: 01 / 04 / 2025
  */
-
-#include <stdbool.h>
-#include "bid_stack.h"
 
 #include "bid_stack.h"
 
 // Crea una pila vacía
 void createEmptyStack(tStack *S) {
-    S->top = SNULL;  // Inicializamos la pila como vacía
-}
-
-// Verifica si la pila está vacía
-bool isEmptyStack(tStack S) {
-    return S.top == -1;
+    S->top = SNULL;
 }
 
 // Inserta una puja en la cima de la pila
 bool push(tItemS item, tStack *S) {
     if (S->top == MAX_BIDS - 1) {
-        return false; // La pila está llena, no se puede apilar más elementos
+        return false;
     }else {
-        S->top++;              // Mover la cima al siguiente índice
-        S->items[S->top] = item; // Insertar la puja
+        S->top++;
+        S->items[S->top] = item;
         return true;
     }
 }
 
 // Elimina la puja en la cima de la pila
 void pop(tStack *S) {
-    if (!isEmptyStack(*S)) {
-        S->top--; // Mover la cima hacia abajo si la pila no está vacía
-    }
-}
-// Devuelve la puja en la cima sin eliminarla
-tItemS peek(tStack S) {
-    return S.items[S.top]; // Devolvemos el elemento en la cima
+    S->top--;
 }
 
+// Devuelve la puja en la cima sin eliminarla
+tItemS peek(tStack S) {
+    return S.items[S.top];
+}
+
+// Verifica si la pila está vacía
+bool isEmptyStack(tStack S) {
+    return S.top == -1;
+}
