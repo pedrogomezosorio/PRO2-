f9fd44f47cbcb1dc6182471539b6ac949e9f7eaa
Samuel Mouriño <153455065+samuelmourino@users.noreply.github.com>
2025-04-02 13:52:22 +0200

    Update console_list.c

--- console_list.c
+++ console_list.c
@@ -2,8 +2,8 @@
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
  * AUTHOR 1: Samuel Mouriño Cernadas LOGIN 1: samuel.mourino@udc.es
- * AUTHOR 2: Pedro Gómez Osorio LOGIN 2: pedro.gomo
- * GROUP: *2.3*
+ * AUTHOR 2: Pedro Gómez Osorio LOGIN 2: pedro.gomez.osorio@udc.es
+ * GROUP: 2.3
  * DATE: 01 / 04 / 2025
  */
 
