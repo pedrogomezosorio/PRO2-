8b85dbf176e1e658675b7b748be23b33df9da3d3
pedrogomezosorio <pedro.gomez.osorio@udc.es>
2025-04-23 19:54:54 +0200

    Add files via upload

--- P2/main.c
+++ P2/main.c
@@ -17,175 +17,182 @@
 
 #define MAX_BUFFER 255
 
-void newConsole(char *commandNumber, char *param1, char *param2, tList *list) {
+void new(char *commandNumber, char command, char *consoleId, char *userId, char *consoleBrand,
+                char *consolePrice, tList *L)
+{
     tItemL item;
-    if (findItem(param1, *list) == LNULL) {
-        strcpy(item.consoleId, param1);
-        strcpy(item.category, param2);
-        strcpy(item.state, "ACTIVE");
-        createEmptyStack(&item.bidStack);
-        insertItem(item, list);
-        printf("%s ==> New: console %s category %s\n", commandNumber, param1, param2);
+
+    printf("%s %c: console %s seller %s brand %s price %s\n", commandNumber, command, consoleId, userId,
+           consoleBrand, consolePrice);
+
+    if (findItem(consoleId, *L) != LNULL)
+    {
+        printf("+ Error: New not possible\n");
+        return;
+    }
+
+    strcpy(item.consoleId, consoleId);
+    strcpy(item.seller, userId);
+
+    if (strcmp(consoleBrand, "nintendo") == 0)
+        item.consoleBrand = nintendo;
+    else if (strcmp(consoleBrand, "sega") == 0)
+        item.consoleBrand = sega;
+    else {
+        printf("+ Error: New not possible\n");
+        return;
+    }
+
+    item.consolePrice = atof(consolePrice);
+    item.bidCounter = 0;
+    createEmptyStack(&item.bidStack);
+
+    if (insertItem(item, L))
+    {
+        printf("* New: console %s seller %s brand %s price %.2f\n", consoleId, userId,consoleBrand,
+               atof(consolePrice));
     } else {
-        printf("%s ==> + Error: Console already exists\n", commandNumber);
+        printf("+ Error: New not possible\n");
     }
 }
 
-void deleteConsole(char *commandNumber, char *param1, tList *list) {
-    tPosL p = findItem(param1, *list);
-    if (p != LNULL) {
-        deleteAtPosition(p, list);
-        printf("%s ==> Delete: console %s\n", commandNumber, param1);
-    } else {
-        printf("%s ==> + Error: Console not found\n", commandNumber);
-    }
-}
-
-void bidConsole(char *commandNumber, char *param1, char *param2, char *param3, tList *list) {
+void delete(char *commandNumber, char command, char *consoleId, tList *L)
+{
     tItemL item;
-    tItemS bid;
-    tPosL p = findItem(param1, *list);
-    if (p != LNULL) {
-        item = getItem(p, *list);
-        if (strcmp(item.state, "ACTIVE") == 0) {
-            strcpy(bid.bidder, param2);
-            bid.price = atoi(param3);
-            if (push(bid, &item.bidStack)) {
-                updateItem(item, p, list);
-                printf("%s ==> Bid: console %s bidder %s price %d\n", commandNumber, param1, bid.bidder, bid.price);
-            } else {
-                printf("%s ==> + Error: Stack overflow\n", commandNumber);
-            }
-        } else {
-            printf("%s ==> + Error: Inactive console\n", commandNumber);
-        }
-    } else {
-        printf("%s ==> + Error: Console not found\n", commandNumber);
+    tPosL pos;
+    printf("%s %c: console %s\n", commandNumber, command, consoleId);
+
+    pos = findItem(consoleId, *L);
+
+    if (pos == LNULL)
+    {
+        printf("+ Error: Delete not possible\n");
+        return;
+    }
+
+    item = getItem(pos, *L);
+
+    while(!isEmptyStack(item.bidStack))     // vaciamos la pila de pujas para poder eliminar la consola
+    {
+        peek(item.bidStack);
+    }
+
+    deleteAtPosition(pos, L);
+
+    if (item.consoleBrand == 0)
+        printf("* Delete: console %s seller %s brand %s price %.2f bids %d\n",item.consoleId,
+               item.seller, "nintendo", item.consolePrice, item.bidCounter);
+    else
+        printf("* Delete: console %s seller %s brand %s price %.2f bids %d\n",item.consoleId,
+               item.seller, "sega", item.consolePrice, item.bidCounter);
+}
+
+void bid(char *commandNumber, char command, char *consoleId, char *userId, char *consolePrice, tList *L)
+{
+
+}
+
+void award(char *commandNumber, char command, char *consoleId, tList *L)
+{
+
+}
+
+void invalidateBids(char *commandNumber, char command, tList *L)
+{
+
+}
+
+void removeConsole(char *commandNumber, char command, tList *L)
+{
+
+}
+
+void stats(char *commandNumber, char command, tList *L)
+{
+
+}
+
+void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4, tList *L) {
+    switch (command)
+    {
+        case 'N':
+            new(commandNumber, command, param1, param2, param3, param4, L);
+            break;
+
+        case 'D':
+            delete(commandNumber, command, param1, L);
+            break;
+
+        case 'B':
+            bid (commandNumber, command, param1, param2, param3, L);
+            break;
+
+        case 'A':
+            award(commandNumber, command, param1, L);
+            break;
+
+        case 'I':
+            invalidateBids(commandNumber, command, L);
+            break;
+
+        case 'R':
+            removeConsole(commandNumber, command, L);
+            break;
+
+        case 'S':
+            stats(commandNumber, command, L);
+            break;
+
+        default:
+            printf("Unknown command\n");
+            break;
     }
 }
 
-void awardConsole(char *commandNumber, char *param1, tList *list) {
-    tItemL item;
-    tItemS bid;
-    tPosL p = findItem(param1, *list);
-    if (p != LNULL) {
-        item = getItem(p, *list);
-        if (!isEmptyStack(item.bidStack)) {
-            bid = peek(item.bidStack);
-            strcpy(item.state, "AWARDED");
-            updateItem(item, p, list);
-            printf("%s ==> Award: console %s bidder %s price %d\n", commandNumber, param1, bid.bidder, bid.price);
-        } else {
-            printf("%s ==> + Error: No bids\n", commandNumber);
-        }
-    } else {
-        printf("%s ==> + Error: Console not found\n", commandNumber);
-    }
-}
-
-void rejectConsole(char *commandNumber, char *param1, tList *list) {
-    tItemL item;
-    tItemS bid;
-    tPosL p = findItem(param1, *list);
-    if (p != LNULL) {
-        item = getItem(p, *list);
-        if (!isEmptyStack(item.bidStack)) {
-            bid = peek(item.bidStack);
-            strcpy(item.state, "REJECTED");
-            updateItem(item, p, list);
-            printf("%s ==> Reject: console %s bidder %s price %d\n", commandNumber, param1, bid.bidder, bid.price);
-        } else {
-            printf("%s ==> + Error: No bids\n", commandNumber);
-        }
-    } else {
-        printf("%s ==> + Error: Console not found\n", commandNumber);
-    }
-}
-
-void statsConsole(char *commandNumber, tList *list) {
-    if (!isEmptyList(*list)) {
-        tPosL p;
-        tItemL item;
-        for (p = first(*list); p != LNULL; p = next(p, *list)) {
-            item = getItem(p, *list);
-            printf("Console %s category %s state %s bids %d\n",
-                   item.consoleId,
-                   item.category,
-                   item.state,
-                   item.bidStack.top + 1);
-        }
-    } else {
-        printf("%s ==> + Error: No consoles\n", commandNumber);
-    }
-}
-
-void withdrawBid(char *commandNumber, char *param1, tList *list) {
-    tItemL item;
-    tItemS bid;
-    tPosL p = findItem(param1, *list);
-    if (p != LNULL) {
-        item = getItem(p, *list);
-        if (!isEmptyStack(item.bidStack)) {
-            bid = peek(item.bidStack);
-            pop(&item.bidStack);
-            updateItem(item, p, list);
-            printf("%s ==> Withdraw: console %s bidder %s price %d\n", commandNumber, param1, bid.bidder, bid.price);
-        } else {
-            printf("%s ==> + Error: No bids\n", commandNumber);
-        }
-    } else {
-        printf("%s ==> + Error: Console not found\n", commandNumber);
-    }
-}
-
-void processCommand(char *commandNumber, char command, char *param1, char *param2, char *param3, char *param4, tList *list) {
-    switch (command) {
-        case 'N': newConsole(commandNumber, param1, param2, list); break;
-        case 'D': deleteConsole(commandNumber, param1, list); break;
-        case 'B': bidConsole(commandNumber, param1, param2, param3, list); break;
-        case 'A': awardConsole(commandNumber, param1, list); break;
-        case 'R': rejectConsole(commandNumber, param1, list); break;
-        case 'S': statsConsole(commandNumber, list); break;
-        case 'I': withdrawBid(commandNumber, param1, list); break;
-        default: break;
-    }
-}
-
-void readTasks(char *filename, tList *list) {
+void readTasks(char *filename)
+{
     FILE *f = NULL;
     char *commandNumber, *command, *param1, *param2, *param3, *param4;
     const char delimiters[] = " \n\r";
     char buffer[MAX_BUFFER];
 
     f = fopen(filename, "r");
+    tList L;
+    createEmptyList(&L);
 
-    if (f != NULL) {
-        while (fgets(buffer, MAX_BUFFER, f)) {
+    if (f != NULL)
+    {
+        while (fgets(buffer, MAX_BUFFER, f))
+        {
             commandNumber = strtok(buffer, delimiters);
             command = strtok(NULL, delimiters);
             param1 = strtok(NULL, delimiters);
             param2 = strtok(NULL, delimiters);
             param3 = strtok(NULL, delimiters);
             param4 = strtok(NULL, delimiters);
-            processCommand(commandNumber, command[0], param1, param2, param3, param4, list);
+
+            processCommand(commandNumber, command[0], param1, param2, param3, param4, &L);
         }
         fclose(f);
-    } else {
+    }
+    else {
         printf("Cannot open file %s.\n", filename);
     }
 }
 
-int main(int nargs, char **args) {
+int main(int nargs, char **args)
+{
     char *file_name = "new.txt";
+
     if (nargs > 1) {
         file_name = args[1];
     } else {
-#ifdef INPUT_FILE
+        #ifdef INPUT_FILE
         file_name = INPUT_FILE;
-#endif
+        #endif
     }
-    tList list;
-    createEmptyList(&list);
-    readTasks(file_name, &list);
+
+    readTasks(file_name);
+
     return 0;
 }
